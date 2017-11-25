a34dfe3439d0239a50a86dc8db14c500aaedb661
unknown <masonjterry@gmail.com>
2017-11-24 19:57:16 -0600

    updated html css and js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,12 @@
+// when player one name is input, have that name printed in the player one box
+// when player one name is input, have player 2 box print that we are waiting on player 2
+// when player two name in input, print player 2 name in player 2 box
+// when player two name is input, provide the players with rock paper or Scissors
+//
+
 $(document).ready(function() {
 
-// Initialize Firebase
+// Initialize Firebases
 let config = {
   apiKey: "AIzaSyD6gCxB9q8njNgaM6avMMpfo1No2VU5cMI",
   authDomain: "rockpaperscissors-5a354.firebaseapp.com",
@@ -11,26 +17,42 @@ let config = {
 };
 firebase.initializeApp(config);
 
+// variable for database
 let database = firebase.database();
+
+// variables for connections
+    let ourConnectionsRef = database.ref("/connections");
+    let googleConnectionsRef = database.ref(".info/connected");
+    let connection;
+
+    googleConnectionsRef.on("value", function(snapshot) {
+      console.log("snapshot.val googleCon", snapshot.val());
+      if (snapshot.val()) {
+        connection = ourConnectionsRef.push(true);
+        connection.onDisconnect().remove();
+        $("#player-1-box").text("Please enter your name");
+      }
+    });
+
+    ourConnectionsRef.on("value", function(snapshot) {
+      console.log("snapshot.numChildren", snapshot.numChildren());
+    });
 
   $("#player-button").on("click", function(event) {
 
     event.preventDefault();
 
     let player = $("#player-name").val().trim();
-    console.log(player);
+    console.log("player", player);
 
     database.ref().push({ player: player, dateAdded: firebase.database.ServerValue.TIMESTAMP });
 
-    });
+    if (database.player === "") {
+      $("#player-1-box").text(database.player);
+    } else {
+      $("#player-2-box").text(database.player);
+    }
 
-    database.ref().on("child_added", function(snapshot) {
-      console.log(snapshot.val());
-
-      $("#player-1-box").text(snapshot.val().player);
-
-    }, function(errorObject) {
-      console.log("Errors: " + errorsObject.code);
-    });
+  });
 
 });
